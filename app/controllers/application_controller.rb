ec32c27ce9187c23730fa41adb3eb2ba66b5e6c8
Gabriel Escodino <gabrielescodino@gmail.com>
2020-07-05 14:30:11 -0300

    Adjust navbar to allow signout and set current user as a helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   rescue ActiveRecord::RecordNotFound
