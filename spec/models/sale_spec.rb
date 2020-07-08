b8e38efd9cf3109a379a3175ffd123f7c18413e0
Gabriel Escodino <gabrielescodino@gmail.com>
2020-07-08 01:01:50 -0300

    Add spec to calculate income method on Sale.

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Sale, type: :model do
     it { is_expected.to belong_to :customer }
     it { is_expected.to belong_to :item }
   end
+
+  describe '#calculate_income' do
+    it 'calculates income of a sale' do
+      sales_report = create(:sales_report)
+      sale = create(:sale, sales_report: sales_report, item_price: 20000, purchase_count: 10)
+      sale2 = create(:sale,sales_report: sales_report, item_price: 100, purchase_count: 0)
+      sale2 = create(:sale,sales_report: sales_report, item_price: 0, purchase_count: 54232)
+
+      expect(sales_report.sales.calculate_income).to eq(200000)
+    end
+  end
 end
