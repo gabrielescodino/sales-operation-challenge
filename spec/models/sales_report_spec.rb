9ff85100c013dd0e1d64fb504b29e95195b7cd83
Gabriel Escodino <gabrielescodino@gmail.com>
2020-07-08 01:04:49 -0300

    Add spec to enqueue process on SalesReport.

--- spec/models/sales_report_spec.rb
+++ spec/models/sales_report_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe SalesReport, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to :user }
   end
+
+  describe '#enqueue_process!' do
+    subject do
+      input_file = fixture_file_upload(Rails.root.join('spec/fixtures/files/example_input.tab'), 'image/txt')
+      create(:sales_report, input_file: input_file)
+    end
+
+    it 'updates status to pending' do
+      subject.enqueue_process!
+
+      expect(subject.status).to eq('pending')
+    end
+
+    it 'enqueues SalesImporterJob' do
+      expect { subject.enqueue_process! }.to have_enqueued_job(SalesImporterJob)
+    end
+  end
 end
