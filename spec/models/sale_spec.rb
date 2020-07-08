b282de6f02db074d66b2b775436ddc8f0b242a0e
Gabriel Escodino <gabrielescodino@gmail.com>
2020-07-08 01:02:34 -0300

    Format fixed numbers on spec.

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe Sale, type: :model do
   describe '#calculate_income' do
     it 'calculates income of a sale' do
       sales_report = create(:sales_report)
-      sale = create(:sale, sales_report: sales_report, item_price: 20000, purchase_count: 10)
+      sale = create(:sale, sales_report: sales_report, item_price: 20_000, purchase_count: 10)
       sale2 = create(:sale,sales_report: sales_report, item_price: 100, purchase_count: 0)
-      sale2 = create(:sale,sales_report: sales_report, item_price: 0, purchase_count: 54232)
+      sale2 = create(:sale,sales_report: sales_report, item_price: 0, purchase_count: 54_232)
 
-      expect(sales_report.sales.calculate_income).to eq(200000)
+      expect(sales_report.sales.calculate_income).to eq(200_000)
     end
   end
 end
